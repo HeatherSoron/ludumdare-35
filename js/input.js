0c3f33cc3e5d4f76dda94f8d3b85eefd35acf186
Ethan "Soron" Kaminski <ethan.kaminski@gmail.com>
2016-04-16 14:31:08 +1200

    Ignore key events in a textarea

--- js/input.js
+++ js/input.js
@@ -2,7 +2,7 @@ var keysHeld = {};
 
 function handleKeyDown(e) {
 	var keyName = keyMap[e.keyCode];
-	if (keyName) {
+	if (keyName && e.target.tagName != 'TEXTAREA') {
 		keysHeld[keyName] = true;
 		// e.preventDefault is used here to prevent scrolling when the spacebar is pressed
 		e.preventDefault();
@@ -11,7 +11,7 @@ function handleKeyDown(e) {
 
 function handleKeyUp(e) {
 	var keyName = keyMap[e.keyCode];
-	if (keyName) {
+	if (keyName && e.target.tagName != 'TEXTAREA') {
 		keysHeld[keyName] = false;
 		e.preventDefault();
 	}
